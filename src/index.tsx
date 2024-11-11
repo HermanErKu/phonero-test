b7de81d8309d13c3be8fe158999187ca04e43985
HermanErKu <herman.lindalen.nodeland@outlook.com>
2024-11-11 13:11:20 +0100

    added react router and moved app to index page

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import Index from './Pages/Index';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Index />} />
+        <Route path="/" element={<Index />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+const container = document.getElementById("root");
+if (container) {
+  const root = ReactDOM.createRoot(container);
+  root.render(<App />);
+} else {
+  console.error("Failed to find the root element");
+}
